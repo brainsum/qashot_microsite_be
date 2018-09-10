e5e064f21487e339b3fceab2f4f5c71c1277bc9b
mhavelant <mhavelant@brainsum.com>
2018-09-10 17:00:48 +0200

    Replace MQ calls with resultsClient calls.
    	- Also clean out dummy data.

--- mailer/src/lib/results-processor.js
+++ mailer/src/lib/results-processor.js
@@ -1,11 +1,10 @@
 'use strict';
 
 const db = require('../database');
-const ResultsQueue = require('../message-queue').ResultsQueue;
 const ResultsMailer = require('../mailer').ResultsMailer;
+const resultsClient = require('../client/results');
 
 // We are only enabling firefox for this service until chrome is working properly.
-const currentChannel = ResultsQueue.channelConfigs.firefox;
 const timeout = 10000;
 
 function delay(t, v) {
@@ -14,17 +13,20 @@ function delay(t, v) {
     });
 }
 
+/**
+ * @type {Sequelize.Model}
+ */
+const ResultModel = db.models.Result;
+
 async function storeResults(results) {
-    const uuid = results.metadata.uuid;
+    const uuid = results.original_request.uuid;
 
     let storedResults = undefined;
     try {
-        /**
-         * @type {Sequelize.Model}
-         */
-        const Result = db.models.Result;
-        storedResults = await Result.create({
+        storedResults = await ResultModel.create({
             uuid: uuid,
+            received: true,
+            receivedAt: new Date(),
             rawData: results
         });
     }
@@ -119,14 +121,49 @@ async function storeEmail(result) {
     return Promise.resolve(storedNotification.get({ plain: true }), true);
 }
 
-async function fetchResult() {
+async function getTest() {
+    let test = undefined;
+    try {
+        test = await ResultModel.findOne({
+            where: {
+                received: false
+            }
+        });
+    }
+    catch (error) {
+        return Promise.reject(error);
+    }
 
+    if (null === test) {
+        return Promise.reject('There are no result emails waiting to be sent to users.');
+    }
+
+    return Promise.resolve(test.get({ plain: true }));
+}
+
+async function fetchResult() {
+    let test = undefined;
+    try {
+        test = await getTest();
+    }
+    catch (error) {
+        return Promise.reject(error);
+    }
+
+    let result = undefined;
+    try {
+        result = await resultsClient.getResult(test.uuid);
+    }
+    catch (error) {
+        return Promise.reject(error);
+    }
+
+    return result;
 }
 
 function loop() {
     console.time('resultsProcessorLoop');
-    ResultsQueue.read(currentChannel.name).then(results => {
-    // ResultsQueueReadDummy(currentChannel.name).then(results => {
+    fetchResult().then(results => {
         return storeResults(results);
     })
         .then(results => {
@@ -155,68 +192,3 @@ function loop() {
 module.exports = {
     loop
 };
-
-async function ResultsQueueReadDummy(channelName) {
-    const uuid = require('uuid/v4');
-
-    const currentUuid = uuid();
-
-    return Promise.resolve({
-        "metadata": {
-            "id": "9",
-            "uuid": currentUuid,
-            "mode": "a_b",
-            "stage": null,
-            "browser": "firefox",
-            "engine": "slimerjs",
-            "viewportCount": 2,
-            "scenarioCount": 1,
-            "duration": {
-                "full": {
-                    "start": "2018-08-24T14:25:27.263Z",
-                    "end": "2018-08-24T14:25:53.126Z",
-                    "duration": 25.863,
-                    "metric_type": "seconds"
-                },
-                "reference": {
-                    "start": "2018-08-24T14:25:27.272Z",
-                    "end": "2018-08-24T14:25:40.306Z",
-                    "duration": 13.034,
-                    "metric_type": "seconds"
-                },
-                "test": {
-                    "start": "2018-08-24T14:25:40.307Z",
-                    "end": "2018-08-24T14:25:53.122Z",
-                    "duration": 12.815,
-                    "metric_type": "seconds"
-                }
-            },
-            "testCount": 2,
-            "passedCount": 0,
-            "failedCount": 2,
-            "passRate": 0,
-            "success": false
-        },
-        "results": [
-            {
-                "scenarioLabel": "405bc5b5-6fd3-4b8f-9196-f22331bf3c90",
-                "viewportLabel": "Full HD",
-                "success": false,
-                "reference": "../reference/9_405bc5b5-6fd3-4b8f-9196-f22331bf3c90_0_document_0_Full_HD.png",
-                "test": "../test/20180824-142541/9_405bc5b5-6fd3-4b8f-9196-f22331bf3c90_0_document_0_Full_HD.png",
-                "diffImage": "../test/20180824-142541/failed_diff_9_405bc5b5-6fd3-4b8f-9196-f22331bf3c90_0_document_0_Full_HD.png",
-                "misMatchPercentage": "6.88"
-            },
-            {
-                "scenarioLabel": "405bc5b5-6fd3-4b8f-9196-f22331bf3c90",
-                "viewportLabel": "Mobile",
-                "success": false,
-                "reference": "../reference/9_405bc5b5-6fd3-4b8f-9196-f22331bf3c90_0_document_1_Mobile.png",
-                "test": "../test/20180824-142541/9_405bc5b5-6fd3-4b8f-9196-f22331bf3c90_0_document_1_Mobile.png",
-                "diffImage": "../test/20180824-142541/failed_diff_9_405bc5b5-6fd3-4b8f-9196-f22331bf3c90_0_document_1_Mobile.png",
-                "misMatchPercentage": "6.88"
-            }
-        ],
-        "resultsUrl": "http://www.brainsum.com"
-    });
-}
