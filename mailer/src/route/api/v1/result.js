1322fdade321a1ff9ed1c41092c6e42ace8b8af8
mhavelant <mhavelant@brainsum.com>
2018-09-10 17:22:39 +0200

    Add path for registering mailer listeners.

--- mailer/src/route/api/v1/result.js
+++ mailer/src/route/api/v1/result.js
@@ -13,6 +13,40 @@ const db = require('../../../database');
 const ResultModel = db.models.Result;
 
 /**
+ * Add listener result, so the mailer can try to fetch results for it.
+ */
+router.post('/add-listener', asyncHandler(async function (req, res, next) {
+    const test = req.body;
+
+    if (null === test || 'undefined' === typeof test) {
+        return res.status(400).json({
+            message: 'Empty request body.'
+        });
+    }
+
+
+    let storedResults = undefined;
+    try {
+        storedResults = await ResultModel.create({
+            uuid: test.uuid,
+            received: false,
+            rawData: {}
+        });
+    }
+    catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            error: error
+        });
+    }
+
+    return res.status(201).json({
+        message: 'Created.',
+        response: storedResults
+    });
+}));
+
+/**
  * Return a list of results. Optionally, filter against a given list of UUIDs.
  */
 router.post('/list', asyncHandler(async function (req, res, next) {
