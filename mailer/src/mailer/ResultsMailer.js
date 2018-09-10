fbf705aa5adaa8fe0d836f2853f01ba7ebb18c72
mhavelant <mhavelant@brainsum.com>
2018-09-10 21:21:34 +0200

    Add mailgun support for the mailer.

--- mailer/src/mailer/ResultsMailer.js
+++ mailer/src/mailer/ResultsMailer.js
@@ -2,15 +2,31 @@
 
 const Mailer = require('../lib/mailer');
 
-const config = {
-    pool: true,
-    host: process.env.MAILER_HOST,
-    port: Number(process.env.MAILER_PORT),
-    auth: {
-        user: process.env.MAILER_USER,
-        pass: process.env.MAILER_PASS
-    }
-};
+const transporter = process.env.MAILER_TRANSPORTER;
+
+let config = undefined;
+if ('mailgun' === transporter) {
+    const mailgun = require('nodemailer-mailgun-transport');
+    const mailgunConfig = {
+        auth: {
+            api_key: process.env.MAILER_MAILGUN_API_KEY,
+            domain: process.env.MAILER_MAILGUN_DOMAIN
+        }
+    };
+    config = mailgun(mailgunConfig)
+}
+
+if ('mailtrap' === transporter) {
+    config = {
+        pool: true,
+        host: process.env.MAILER_HOST,
+        port: Number(process.env.MAILER_PORT),
+        auth: {
+            user: process.env.MAILER_USER,
+            pass: process.env.MAILER_PASS
+        }
+    };
+}
 
 const ResultsMailer = new Mailer('ResultsMailer', config);
 
