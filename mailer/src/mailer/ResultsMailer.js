c27f35fc1a505c3b0e87e85d45be0460a665fa6e
mhavelant <mhavelant@brainsum.com>
2018-09-12 22:19:47 +0200

    Add rudimentary SMTP server.

--- mailer/src/mailer/ResultsMailer.js
+++ mailer/src/mailer/ResultsMailer.js
@@ -28,6 +28,23 @@ if ('mailtrap' === transporter) {
     };
 }
 
+if ('microsite_smtp_server' === transporter) {
+    config = {
+        pool: true,
+        host: process.env.MAILER_HOST,
+        port: Number(process.env.MAILER_PORT),
+        auth: {
+            user: process.env.MAILER_USER,
+            pass: process.env.MAILER_PASS
+        },
+        secure: false,
+        tls: {
+            rejectUnauthorized: false
+        },
+        debug: true
+    };
+}
+
 const ResultsMailer = new Mailer('ResultsMailer', config);
 
 module.exports = ResultsMailer;
