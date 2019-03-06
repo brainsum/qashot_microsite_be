2e4626b9cd8de1442d7f0df2bdfe3a75b7f765c5
mhavelant <mhavelant@brainsum.com>
2019-03-06 13:24:07 +0100

    QAP-000 | Add helper script for checking npm for updates.

---
+++ check-npm-updates.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+services=('web' 'queue' 'mailer')
+
+for service in ${services[@]}
+do
+  echo "NPM status for ${service}:"
+  cd "${service}" && npm outdated -l
+  cd ..
+  echo ""
+done
